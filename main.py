3dd16dc348d6934d8e71312b85f3ab3ac54ac20b
KarlStazysta <mlynekarol@gmail.com>
2023-10-10 00:24:32 +0200

    added tkinter

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sounddevice as sd
-from scipy.io.wavfile import write
-import wavio as wv
-freq = 44100
-duration = 5
-recording = sd.rec(int(duration*freq), samplerate=freq, chanels=2)
-sd.wait()
-write("recording0.wav", freq, recording)
-wv.write("recording1.wav", recording, freq, sampwidth=2)
+import soundfile as sf
+from tkinter import *
+
+def Voice_rec():
+    freq = 44100
+    duration = 5
+    recording = sd.rec(int(duration*freq), samplerate=freq, channels=2)
+    sd.wait()
+    return sf.write('my_Audio_file.flac', recording, freq)
+master = Tk()
+Label(master, text=" Voice Recorder  :").grid(row=0, sticky=W, rowspan=5)
+b = Button(master, text="Start", command=Voice_rec)
+b.grid(row=0, column=2, columnspan=2, rowspan=2, padx=5, pady=5)
+mainloop()
